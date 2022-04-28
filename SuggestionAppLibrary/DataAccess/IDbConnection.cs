e7b48859da675a994eebf79ebf7474affb7dcf69
Patryk Sładek <patryksladek.dev@gmail.com>
2022-04-28 08:55:03 +0200

    Database Connection Class

---
+++ SuggestionAppLibrary/DataAccess/IDbConnection.cs
@@ -0,0 +1,15 @@
+﻿namespace SuggestionAppLibrary.DataAccess;
+
+public interface IDbConnection
+{
+   IMongoCollection<CategoryModel> CategoryCollection { get; }
+   string CategoryCollectionName { get; }
+   MongoClient Clinet { get; }
+   string DbName { get; }
+   string StatusCollecionName { get; }
+   IMongoCollection<StatusModel> StatusCollection { get; }
+   string SuggestionCollecionName { get; }
+   IMongoCollection<SuggestionModel> SuggestionCollection { get; }
+   string UserCollecionName { get; }
+   IMongoCollection<UserModel> UserCollection { get; }
+}
