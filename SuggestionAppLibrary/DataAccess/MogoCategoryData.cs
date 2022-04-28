197f282ec59d80099efc8257b2895e9078358096
Patryk Sładek <patryksladek.dev@gmail.com>
2022-04-28 08:57:00 +0200

    Category Data Access

---
+++ SuggestionAppLibrary/DataAccess/MogoCategoryData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MogoCategoryData : ICategoryData
+{
+   private readonly IMongoCollection<CategoryModel> _categories;
+   private readonly IMemoryCache _cache;
+   private readonly string cacheName = "CategoryData";
+
+   public MogoCategoryData(IDbConnection db, IMemoryCache cache)
+   {
+      _cache = cache;
+      _categories = db.CategoryCollection;
+   }
+
+   public async Task<IEnumerable<CategoryModel>> GetAllCategoriesAsync()
+   {
+      var output = _cache.Get<IEnumerable<CategoryModel>>(cacheName);
+      if (output is null)
+      {
+         var results = await _categories.FindAsync(_ => true);
+         output = await results.ToListAsync();
+
+         _cache.Set(cacheName, output, TimeSpan.FromDays(1));
+      }
+
+      return output;
+   }
+
+   public async Task CreateCategory(CategoryModel category)
+   {
+      await _categories.InsertOneAsync(category);
+   }
+}
